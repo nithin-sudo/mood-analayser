4bc4d257efb4ab4052995408218704f954146752
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-13 21:06:19 +0530

    Added: analyseMood method which returns sad or happy.

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,6 +2,11 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser
 {
+    /**
+     * Ability to check the given string in the given message
+     * @param message
+     * @return
+     */
     public String analyseMood(String message) {
         if (message.contains("Sad")) {
             return "SAD";
