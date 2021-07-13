d32beccd499332bc1079a0357e34b2825a0bb25b
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-13 21:10:39 +0530

    Added: test case which is excpecting HAPPY.

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -7,8 +7,14 @@ public class MoodAnalyserTest
 {
     @Test
     public void givenMessage_WhenSad_ShouldReturn_Sad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am in Sad Mood");
         Assertions.assertEquals("SAD", mood);
+    }
+    @Test
+    public void givenMessage_whenNotSad_shouldReturn_Happy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(); 
+        String mood = moodAnalyser.analyseMood("I am in any Mood");
+        Assertions.assertEquals("HAPPY", mood);
     }
 }
