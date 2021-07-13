6988c787a58f563572c0c5e41f66940150e4c3f3
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-13 21:01:00 +0530

    Added: analyseMood method and returning sad or happy.

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,6 +1,14 @@
 package com.bridgelabz.moodanalyser;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest
 {
-
+    @Test
+    public void givenMessage_WhenSad_ShouldReturn_Sad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        Assertions.assertEquals("SAD", mood);
+    }
 }
